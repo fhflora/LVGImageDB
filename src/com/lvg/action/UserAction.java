aa82b71975d329b562a217c44404d3e635135ee0
fuzhifr <fuzhifr@gmail.com>
2014-07-14 16:33:47 +0800

    用户根据身份 登陆到不同的页面

--- src/com/lvg/action/UserAction.java
+++ src/com/lvg/action/UserAction.java
@@ -15,8 +15,8 @@ public class UserAction extends ActionSupport implements SessionAware{
 	private Map<String,Object> session;
 	private String certCode;
 	private UserManage userManage;
-	
-	public String CheckLoginAction(){
+
+	public String userLogin(){
 		String  message="验证码错误，请重新输入！";
 		if(!certCode.equals(ServletActionContext.getRequest().getSession().getAttribute("rand")))
 		{
@@ -33,24 +33,82 @@ public class UserAction extends ActionSupport implements SessionAware{
 		}
 		//添加访问数据库语句  验证用户
 		
-		userManage=new UserManage(GetConnection.getConn());
+		//userManage=new UserManage(GetConnection.getConn());
 		
-		
-		if(userManage.loginCheck("fz", "fz123", 1)!=-1){
-			user=(UserInfo) userManage.getUserInfo(userManage.loginCheck("fz", "fz123", 1));
+		/*if(userManage.loginCheck(user.getUserName(), user.getPassword(), user.getUserType())!=-1){
+			user=(UserInfo) userManage.getUserInfo(userManage.loginCheck(user.getUserName(), user.getPassword(), user.getUserType()));*/
+			System.out.println(user.getUserName());
+			System.out.println(user.getPassword());
+		if(user.getUserName().equals("fz")){
 			user.setState((byte) 0);
+			//userManage.setUserInfo(user);
 			session.put("user", user);
 			session.put("username", user.getUserName());
 			session.put("lvg_login", true);
+			session.put("permission",user.getPermission());
+			System.out.println("成功");
 			return SUCCESS;
 		}else{
 			message="用户名或者密码错误";
 			this.addActionMessage(message);
 			return INPUT;
 		}
-		
 	}
-
+	
+	public String managerLogin(){
+		String  message="验证码错误，请重新输入！";
+		if(!certCode.equals(ServletActionContext.getRequest().getSession().getAttribute("rand")))
+		{
+			this.addActionMessage(message);
+			return INPUT;
+		}
+		if(user.getUserName().isEmpty()){
+			this.addFieldError("userName", "用户名不能为空");
+			return INPUT;
+		}
+		if(user.getPassword().isEmpty()){
+			this.addFieldError("userName", "用户名不能为空");
+			return INPUT;
+		}
+		//添加访问数据库语句  验证用户
+		
+		//userManage=new UserManage(GetConnection.getConn());
+		
+	/*	if(userManage.loginCheck(user.getUserName(), user.getPassword(), user.getUserType())!=-1){
+			user=(UserInfo) userManage.getUserInfo(userManage.loginCheck(user.getUserName(), user.getPassword(), user.getUserType()));*/
+		System.out.println(user.getUserName());
+		System.out.println(user.getPassword());
+		if(user.getUserName().equals("fz")){
+			user.setState((byte) 0);
+			//userManage.setUserInfo(user);
+			session.put("user", user);
+			session.put("username", user.getUserName());
+			session.put("lvg_login", true);
+			session.put("permission",user.getPermission());
+			System.out.println("成功");
+			return SUCCESS;
+		}else{
+			message="用户名或者密码错误";
+			this.addActionMessage(message);
+			return INPUT;
+		}
+	}
+	/**
+	 * 退出函数 修改用户状态
+	 * @return
+	 */
+	public String logoutAction(){
+		
+		userManage=new UserManage(GetConnection.getConn());
+		user=(UserInfo)session.get("user");
+		user.setState((byte) 1);
+		if(userManage.setUserInfo(user)){
+			return SUCCESS;
+		}else{
+			session.clear();
+			return INPUT;
+		}
+	}
 	@Override
 	public void setSession(Map<String, Object> session) {
 		this.session = session;		
@@ -75,6 +133,5 @@ public class UserAction extends ActionSupport implements SessionAware{
 	public void setCertCode(String certCode) {
 		this.certCode = certCode;
 	}
-	
 	
 }
